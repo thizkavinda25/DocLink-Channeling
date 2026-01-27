c47e4cb592c86344ec8984c0c2c5e63fddf1d95c
thizkavinda25 <thizarakavinda@gmail.com>
2026-01-27 15:00:24 +0530

    Users Appointment screen setup

--- lib/screens/my_appointment.dart
+++ lib/screens/my_appointment.dart
@@ -9,7 +9,7 @@ class MyAppointment extends StatefulWidget {
 }
 
 class _MyAppointmentState extends State<MyAppointment> {
-  int _selectedTab = 0; 
+  int _selectedTab = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -94,14 +94,142 @@ class _MyAppointmentState extends State<MyAppointment> {
               ),
             ),
             const SizedBox(height: 20),
-            Expanded(
-              child: _selectedTab == 0
-                  ? const Center(child: Text('Upcoming Appointments'))
-                  : const Center(child: Text('Past Appointments')),
-            ),
+            _selectedTab == 0
+                ? Container(
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: _doctorDetails(),
+                  )
+                : Text('data'),
           ],
         ),
       ),
     );
   }
+}
+
+Widget _doctorDetails() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Container(
+        width: double.infinity,
+        height: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://media.istockphoto.com/id/1342134434/photo/caring-doctor-listens-to-patient.jpg?s=612x612&w=0&k=20&c=oTy-3urXPXBh_7oNGQS-mbleCsKpcxGz_-WLcL40EMk=',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Dr. Aris Thorne',
+              style: TextStyle(
+                color: CustomColors.primaryColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Wrap(
+              spacing: 10,
+              children: [
+                Text(
+                  'Cardiologist',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 29, 50, 80),
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  'Heart & Vascular Center',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 29, 50, 80),
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Wrap(
+              spacing: 15,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.event, color: Colors.grey.shade700, size: 15),
+                    SizedBox(width: 3),
+                    Text(
+                      'Jan 27, 2026',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 42, 73, 117),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.access_time_filled,
+                      color: Colors.grey.shade700,
+                      size: 15,
+                    ),
+                    SizedBox(width: 3),
+                    Text(
+                      '10:30 AM',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 42, 73, 117),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: CustomColors.primaryColor,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.event, color: Colors.white, size: 22),
+                        SizedBox(width: 5),
+                        Text(
+                          'Add to Calendar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete, color: Colors.red),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
 }
